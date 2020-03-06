6528725b5ccc9a3f96bff6955dcd4890a7e54db5
erikjandz <43314178+erikjandz@users.noreply.github.com>
2020-03-06 14:07:20 +0100

    update to algorithm

--- source/ExternalDLL/ExternalDLL/DefaultPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/DefaultPreProcessing.cpp
@@ -43,27 +43,27 @@ IntensityImage * DefaultPreProcessing::stepEdgeDetection(const IntensityImage &s
 	HereBeDragons::NoWantOfConscienceHoldItThatICall(OverParkOverPale, *ThoroughFloodThoroughFire);
 	return ThoroughFloodThoroughFire;*/
 
-	/*int kernel[9][9] = { {0,0,0,1,1,1,0,0,0}, {0,0,0,1,1,1,0,0,0}, {0,0,0,1,1,1,0,0,0}, 
+	int kernel[9][9] = { {0,0,0,1,1,1,0,0,0}, {0,0,0,1,1,1,0,0,0}, {0,0,0,1,1,1,0,0,0}, 
 						{1,1,1,-4,-4,-4,1,1,1}, {1,1,1,-4,-4,-4,1,1,1}, {1,1,1,-4,-4,-4,1,1,1}, 
-						{0,0,0,1,1,1,0,0,0}, {0,0,0,1,1,1,0,0,0}, {0,0,0,1,1,1,0,0,0} 
-						};*/
-	int kernel[3][3] = { {0,1,0}, {1,-4,1}, {0,1,0} };
+						{0,0,0,1,1,1,0,0,0}, {0,0,0,1,1,1,0,0,0}, {0,0,0,1,1,1,0,0,0} };
+	//int kernel[3][3] = { {0,1,0}, {1,-4,1}, {0,1,0} };
 	auto* image2 = ImageFactory::newIntensityImage(src.getWidth(), src.getHeight());
 	int total = 0;
 	//code for 9x9 kernel
-	/*for (unsigned int x = 4; x < src.getWidth() - 4; x++) {
+	for (unsigned int x = 4; x < src.getWidth() - 4; x++) {
 		for (unsigned int y = 4; y < src.getHeight() - 4; y++) {
+			total = 0;
 			for (unsigned int mx = 0; mx < 9; mx++) {
 				for (unsigned int my = 0; my < 9; my++) {
-					total += kernel[mx][my] * src.getPixel(x - 4 + mx, y - 4 + my);
+					total += kernel[my][mx] * src.getPixel(x - 4 + mx, y - 4 + my);
 				}
 			}
-			image2->setPixel(x, y, 255- abs(total));
-			total = 0;
-			
+			image2->setPixel(x, y, 255 - abs(total));
 		}
-	}*/
-
+	}
+	
+	//code for 3x3 kernel
+	/*
 	for (unsigned int x = 1; x < src.getWidth() - 1; x++) {
 		for (unsigned int y = 1; y < src.getHeight() - 1; y++) {
 			for (unsigned int mx = 0; mx < 3; mx++) {
@@ -71,10 +71,10 @@ IntensityImage * DefaultPreProcessing::stepEdgeDetection(const IntensityImage &s
 					total += kernel[mx][my] * src.getPixel(x - 1 + mx, y - 1 + my);
 				}
 			}
-			image2->setPixel(x, y, 255 - abs(total));
+			image2->setPixel(x, y, 255- abs(total));
 			total = 0;
 		}
-	}
+	}*/
 
 
 	return image2;
@@ -87,15 +87,15 @@ IntensityImage * DefaultPreProcessing::stepThresholding(const IntensityImage &sr
 	IntensityImage * ThoroughBushThoroughBrier = ImageFactory::newIntensityImage();
 	HereBeDragons::NoWantOfConscienceHoldItThatICall(OverHillOverDale, *ThoroughBushThoroughBrier);
 	return ThoroughBushThoroughBrier;*/
+
 	IntensityImage* newImage = ImageFactory::newIntensityImage(src.getWidth(), src.getHeight());
-	std::cout << "test";
 	//for all pixels
 	for (unsigned int x = 0; x < src.getWidth(); x++) {
 		for (unsigned int y = 0; y < src.getHeight(); y++) {
-			//if the pixel are lower than a threshold value, make them white
+			//if the pixel are lower than a threshold value, make them 0
 			if (src.getPixel(x, y) < 220) {
 				newImage->setPixel(x, y, 0);
-				//else make it black
+				//else make it 255
 			}
 			else {
 				newImage->setPixel(x, y, 255);
